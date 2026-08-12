764efd8fa3f69876de42f23491274932ef22af5f
Sean Vishnev <sean.vishnev@gmail.com>
2026-08-12 20:48:59 +0300

    Still working and adding on function frame and print checks

--- boot/Kernel.asm
+++ boot/Kernel.asm
@@ -7,18 +7,25 @@ print:
     push bp
     mov bp, sp
     
+    mov ah, [bp + 6]
     mov si, [bp + 4]
+    
+    mov bx, 0xB800
+    mov es, bx 
+    
+    mov di, [cursor]
     .print:    
         lodsb
         cmp al, 0
         je .done
         
         mov byte [es:di], al
-        mov byte [es:di + 1], 0x07
+        mov byte [es:di + 1], ah
         add di, 2  
     jmp .print
     
     .done:
+        mov [cursor], di
         mov sp, bp
         pop bp 
         ret
@@ -33,21 +40,18 @@ start:
     
     sti
     
-    mov ax, 0xB800
-    mov es, ax 
-    
     xor ax, ax
     mov DS, ax
     
-    mov di, 0 
-    
+    push 0x07
     push message
     call print
-    add sp, 2
+    add sp, 4
 
 halt:
     cli
     hlt
 
+cursor dw 0
 message db "Hello from the kernel!", 0
 times 1536-($-$$) db 0
