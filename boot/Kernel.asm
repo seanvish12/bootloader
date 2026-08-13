22c6dbb351c09ec80b93d66fed92cf11ba0701f8
Sean Vishnev <sean.vishnev@gmail.com>
2026-08-13 11:34:10 +0300

    Print function supports new line and prevents overflow of characters on screen.

--- boot/Kernel.asm
+++ boot/Kernel.asm
@@ -19,13 +19,39 @@ print:
         cmp al, 0
         je .done
         
+        cmp al, 10
+        je .new_line
+        
         mov byte [es:di], al
         mov byte [es:di + 1], ah
-        add di, 2  
+        add di, 2
+        
+        inc word [cursor_col]
+        cmp word [cursor_col], 80
+        jae .reset_col
+        
+        cmp di, 4000
+        jae .reset
+          
     jmp .print
     
+    .new_line:
+        mov cx, 80              ; 80 characters per line
+        sub cx, [cursor_col]    ; characters remaining on this line
+        shl cx, 1               ; each character = 2 bytes
+        add di, cx              ; move DI to start of next line
+
+        mov [cursor_col], 0
+        jmp .print
+    
+    .reset:
+        xor di, di
+    .reset_col:
+        mov [cursor_col], 0
+        jmp .print
+    
     .done:
-        mov [cursor], di
+        mov word [cursor], di
         mov sp, bp
         pop bp 
         ret
@@ -53,5 +79,6 @@ halt:
     hlt
 
 cursor dw 0
-message db "Hello from the kernel!", 0
+cursor_col dw 0
+message db "Hello from the kernel!", 10, "nigger balls", 0
 times 1536-($-$$) db 0
